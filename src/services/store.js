9159c598585e34f5129c5c24b550f638deb0a075
ayyappa gollu <50751386+ayyappag10@users.noreply.github.com>
2019-10-14 18:09:52 +0530

    made cart info persistent by storing it in local storagee

--- src/services/store.js
+++ src/services/store.js
@@ -5,5 +5,11 @@ import thunk from "redux-thunk";
 
 const store = createStore(Reducer, applyMiddleware(thunk));
 
+store.subscribe(()=>{
+    let state = store.getState();
+    localStorage.setItem("cartProducts", JSON.stringify(state.cart.products));
+    localStorage.setItem("cartTotalInfo", JSON.stringify({totalNoOfItems:state.total.items, totalCost:state.total.cost}))
+})
+
 export default store;
 
