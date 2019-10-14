b3b8238b857987354a9c181ec0d737b39edd1552
ayyappa gollu <50751386+ayyappag10@users.noreply.github.com>
2019-10-14 18:12:07 +0530

    added persitent data support using local storage

--- src/services/cartupdate/reducer.js
+++ src/services/cartupdate/reducer.js
@@ -1,19 +1,17 @@
-import {UDPATE_CART} from "./actiontypes"
-import {ADD_PRODUCT} from "./actiontypes"
-import {REMOVE_PRODUCT} from "./actiontypes"
-const cartReducer = function (state = {products:[]}, action) 
-{    
-    switch(action.type)
+import {UPDATE_TOTAL} from "./actionTypes"
+
+const totalReducer = (state= 
     {
-        case UDPATE_CART:
-            return({...state, products:[...action.payload]})
-                case ADD_PRODUCT:
-                     return({...state, products:[...state.products, action.payload]})
-                    case REMOVE_PRODUCT:
-                        return({...state, products:[...state.products, action.payload]})
-                        default:
-                            return state;
+     items: JSON.parse(localStorage.getItem("cartTotalInfo")).totalNoOfItems,
+     cost: JSON.parse(localStorage.getItem("cartTotalInfo")).totalCost
+    }, action)=>{
+    switch(action.type){
+
+        case UPDATE_TOTAL:
+        return Object.assign({}, state, {...action.payload} )
+        default:
+            return state; 
     }
 }
 
-export default cartReducer;
+export default totalReducer;
