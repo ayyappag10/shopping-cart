1fc7bbd1df8db208b20d7dcc374ce3e9536b3549
ayyappa gollu <50751386+ayyappag10@users.noreply.github.com>
2019-10-20 13:35:39 +0530

    null check for localstorage

--- src/services/cartupdate/reducer.js
+++ src/services/cartupdate/reducer.js
@@ -1,17 +1,20 @@
-import {UPDATE_TOTAL} from "./actionTypes"
+import { UDPATE_CART } from "./actiontypes";
+import { ADD_PRODUCT } from "./actiontypes";
+import { REMOVE_PRODUCT } from "./actiontypes";
+const cartReducer = function(
+  state = { products: localStorage.getItem("cartProducts") ?  [...JSON.parse(localStorage.getItem("cartProducts"))]: [] },
+  action
+) {
+  switch (action.type) {
+    case UDPATE_CART:
+      return { ...state, products: [...action.payload] };
+    case ADD_PRODUCT:
+      return { ...state, products: [...state.products, action.payload] };
+    case REMOVE_PRODUCT:
+      return { ...state, products: [...state.products, action.payload] };
+    default:
+      return state;
+  }
+};
 
-const totalReducer = (state= 
-    {
-     items: JSON.parse(localStorage.getItem("cartTotalInfo")).totalNoOfItems,
-     cost: JSON.parse(localStorage.getItem("cartTotalInfo")).totalCost
-    }, action)=>{
-    switch(action.type){
-
-        case UPDATE_TOTAL:
-        return Object.assign({}, state, {...action.payload} )
-        default:
-            return state; 
-    }
-}
-
-export default totalReducer;
+export default cartReducer;
